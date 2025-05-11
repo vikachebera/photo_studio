35ee164f5318b1951b839c8d038ec9cd61d5cfdb
Viktoriia Chebera <cheberavika@gmail.com>
2025-05-11 16:26:39 +0200

    added         },

---
+++ docs/sidebars.ts
@@ -0,0 +1,37 @@
+import type {SidebarsConfig} from '@docusaurus/plugin-content-docs';
+
+
+/**
+ * Creating a sidebar enables you to:
+ - create an ordered group of docs
+ - render a sidebar for each doc of that group
+ - provide next/previous navigation
+
+ The sidebars can be generated from the filesystem, or explicitly defined here.
+
+ Create as many sidebars as you want.
+ */
+
+const sidebars: SidebarsConfig = {
+
+    tutorialSidebar: [
+        {
+            type: 'doc',
+            id: 'README',
+            label: 'Про проєкт',
+
+        },
+        {
+            type: 'doc',
+            id: 'privacy-policy',
+            label: 'Політика конфіденційності',
+        },
+        {
+            type: 'doc',
+            id: 'LICENSE.md',
+            label: 'Ліцензія',
+        }
+    ],
+};
+
+export default sidebars;
